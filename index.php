cf902149c94078c70d54803dd5e2ae6881a387f7
aschles4 <andsch08252@gmail.com>
2017-05-01 18:39:34 -0400

    First Submission

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+    <title>
+        Hacking Game
+    </title>
+    <body>
+        <header>
+           
+        </header>
+        <main>
+            <div id="install">
+                <h1>Installation and Setup</h1>
+                <ul>
+                    <li>
+                        <h3>Install Virtual Box</h3>
+                        <p>Instructions here</p>
+                    </li>
+                    <li>
+                        <h3>Intall Kali Linux Virtual Machine</h3>
+                        <p>Instructions here</p>
+                    </li>
+                    <li>
+                        <h3>Install Metasploitable 2</h3>
+                        <p>Instructions here</p>
+                    </li>
+                    <li>
+                        <h3>Setting up a secure network connection</h3>
+                        <p>Instructions here</p>
+                    </li>
+                    <li>
+                        <h3>All Good!</h3>
+                        <p>Instructions here</p>
+                    </li>
+                </ul>
+            </div>
+            <div id="Rules">
+                <h1>Welcome to the basics of Hacking Challenge Game</h1>
+                <h3>Rules:</h3>
+                <ul>
+                    <li>
+                        <p>Attacks should only be done within the virtual machine environment</p>
+                    </li>
+                    <li>
+                        <p>This is for educational and ethical hacking purpouses! Use this knowlegde at your own risk!</p>
+                        
+                    </li>
+                </ul>
+            </div>
+            <div id="levels">
+                <h3>Levels:</h3>
+                <ul>
+                    <li>
+                        <h4>Level One (scanning)</h4>
+                    </li>
+                    <li>
+                        <h4>Level Two (XSS)</h4>
+                    </li>
+                    <li>
+                        <h4>Level Three (SQl Injection)</h4>
+                    </li>
+                    <li>
+                        <h4>Level Four (Password cracking)</h4>
+                    </li>
+                    <li>
+                        <h4>Level Five (Challege)</h4>
+                    </li>
+                    
+                </ul>
+            </div>
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
